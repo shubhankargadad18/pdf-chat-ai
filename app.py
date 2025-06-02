99f43ad35c8efcff481a41a34c5541af1bc92ad7
Shubhankar Gadad <shubhankargadad967@gmail.com>
2025-06-02 13:59:57 +0530

    Updating the import for langchain text splitters

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import langchain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain import OpenAI, VectorDBQA
